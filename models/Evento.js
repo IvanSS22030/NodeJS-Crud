74665daba7e2ef83724caede5b0a57d34a81eea8
Ivan joel Sanchez Santana <ivanj_ss@hotmail.com>
2023-11-18 01:49:57 -0400

    almost done

--- models/Evento.js
+++ models/Evento.js
@@ -22,5 +22,10 @@ end:{
 
 });
 
+EventoSchema.method("toJSON", function(){
+const{ __v,_id,...object} = this.toObject();
+object.id = _id;
+return object;
+})
 
 module.exports = model('Evento', EventoSchema);
